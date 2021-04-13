83b8d5dfb4f010b14c1112caf232288f812d69ca
aki21j <ankitgupta21j@gmail.com>
2021-04-13 11:33:25 +0530

    refactoring

--- bin/main.py
+++ bin/main.py
@@ -17,19 +17,42 @@ import time
 def dead_state(width, height):
   return [ [0] * width ] * height
 
+def get_board_dimensions(board):
+  width = len(board)
+  height = len(board[0])
+  return width, height
 
 def random_state(width, height):
-  board_state = dead_state(width, height)
+  # board_state = dead_state(width, height)
 
-  for row in range(len(board_state)):
-    for col in range(len(board_state[row])):
-      random_no = random.random()
-      if random_no >= 0.85:
-        board_state[row][col] = 1
-      else:
-        board_state[row][col] = 0
+  # for row in range(len(board_state)):
+  #   for col in range(len(board_state[row])):
+  #     random_no = random.random()
+  #     if random_no >= 0.85:
+  #       board_state[row][col] = 1
+  #     else:
+  #       board_state[row][col] = 0
 
-  # board_state = [[0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0], [0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0], [0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1], [0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0], [0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1], [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1], [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0], [0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0], [0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0]]
+  board_state = [[0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0],
+ [0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+ [0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1],
+ [0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+ [0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+ [0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1],
+ [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+ [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+ [0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0],
+ [0, 0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0]]
   return board_state
 
 
@@ -48,64 +71,65 @@ def render(initial_board_state):
     
   print("\n".join(lines))
 
+def get_new_cell_value(row, col, initial_state):
+  cell_neighbour_population = 0
+  range_start = {
+    row: (row - 1),
+    col: (col - 1)
+  }
+  range_end = {
+    row: (row + 1),
+    col: (col + 1)
+  }
+
+  board_width, board_height = get_board_dimensions(initial_state)
+  return_val = 0
+  # print(row, col)
+  for i in range(range_start[row], range_end[row] + 1):
+    if i < 0 or i >= board_width:
+      continue
+    for j in range(range_start[col], range_end[col] + 1):
+      if j < 0 or j >= board_height:
+        continue
+      if i == row and j == col:
+        continue
+
+      if initial_state[i][j] == 1:
+        cell_neighbour_population += 1
+
+  if initial_state[row][col] == 1:
+    if cell_neighbour_population <= 1:
+      return_val = 0
+    elif cell_neighbour_population <= 3:
+      return_val = 1
+    else:
+      return_val = 0
+  else:
+    if cell_neighbour_population == 3:
+      return_val = 1
+    else:
+      return_val = 0
+
+  return return_val
+
 
 def next_board_state(initial_board_state):
-  board_state = copy.deepcopy(initial_board_state)
-  for row in range(len(board_state)):
-    for col in range(len(board_state[row])):
-      cell_neighbour_population = 0
-      neighbours = [0] * 8
-      if row != 0:
-        if col != 0:
-          neighbours[0] = initial_board_state[row - 1][col - 1]
-          cell_neighbour_population += initial_board_state[row - 1][col - 1]
-        
-        neighbours[1] = initial_board_state[row - 1][col]
-        cell_neighbour_population += initial_board_state[row - 1][col]
-
-        if col != len(board_state[row])-1:
-          neighbours[2] = initial_board_state[row - 1][col + 1]
-          cell_neighbour_population += initial_board_state[row - 1][col + 1]
-      
-      if col != 0:
-        neighbours[3] = initial_board_state[row][col - 1]
-        cell_neighbour_population += initial_board_state[row][col - 1]
-
-      if col != len(board_state[row])-1:
-        neighbours[4] = initial_board_state[row][col + 1]
-        cell_neighbour_population += initial_board_state[row][col - 1]
-
-      if row != len(board_state)-1:
-        neighbours[5] = initial_board_state[row + 1][col - 1]
-        cell_neighbour_population += initial_board_state[row + 1][col - 1]
-        
-        neighbours[6] = initial_board_state[row + 1][col]
-        cell_neighbour_population += initial_board_state[row + 1][col]
-
-
-        if col != len(board_state[row])-1:
-          neighbours[7] = initial_board_state[row + 1][col + 1]
-          cell_neighbour_population += initial_board_state[row + 1][col + 1]
-
-      cell_population = sum(neighbours)
-
-      # print(row, col, board_state[row][col], cell_neighbour_population)
-      if board_state[row][col] == 1:
-        if cell_population <= 1 or cell_population > 3:
-          board_state[row][col] = 0
-      elif board_state[row][col] == 0 and cell_population == 3:
-          board_state[row][col] = 1
-
-  return board_state
+  board_width, board_height = get_board_dimensions(initial_board_state)
+  new_state = dead_state(board_width, board_height)
+  for row in range(0,board_width):
+    for col in range(0,board_height):
+      new_state[row][col] = get_new_cell_value(row, col, initial_board_state)
+  return new_state
 
 
 def main():
-  board_state = random_state(5, 5)
+  board_state = random_state(20, 20)
   next_state = board_state
   i = 0
-  while True:
-    render(next_state)
-    # print(next_state)
+  while i <= 1:
+    # render(next_state)
+    pprint(next_state)
+    print('------------')
     next_state = next_board_state(next_state)
     time.sleep(0.03)
     i+=1
